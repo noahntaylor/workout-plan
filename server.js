58087be9d4fb163d3925e5bf73775f46201f56ac
Noah Taylor <noahnt@live.ca>
2022-04-21 07:40:42 -0230

    Initial Deploy

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use(require("./routes/workouts"));
 // Get database connection
 const db = require("./db/conn");
 
+// Serve React app
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 // Connect to database when server starts
 app.listen(port, () => {
   db.connectToServer(function (error) {
